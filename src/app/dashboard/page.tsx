c8a7d0dacc6cdad6aa3db917c5d78b21dd3d9533
hnifalii <ankso30561@gmail.com>
2024-12-30 22:03:36 +0700

    Modify some profile page adn testing dummy data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,40 +1,9 @@
-'use client'
+"use client";
 
+import ProfilePostCard from "@/components/ProfilePostCard";
 import Link from "next/link";
 import { useState } from "react";
-
-const Postingan = () => {
-  return (
-    <div>
-      <div className="flex pb-4 gap-2">
-        <h4 className="w-24">Foto</h4>
-        <div className="flex flex-col flex-1">
-          <h5 className="text-[10px] font-semibold pb-1 border-b-2">Update kondisi banjir kanal saat ini</h5>
-          <p className="text-[8px] ">19 jam yang lalu</p>
-          <p className="pt-3 text-[10px]">Baca selengkapnya...</p>
-        </div>
-      </div>
-
-      <div className="flex pb-4 gap-2">
-        <h4 className="w-24">Foto</h4>
-        <div className="flex flex-col flex-1">
-          <h5 className="text-[10px] font-semibold pb-1 border-b-2">Update kondisi banjir kanal saat ini</h5>
-          <p className="text-[8px] ">19 jam yang lalu</p>
-          <p className="pt-3 text-[10px]">Baca selengkapnya...</p>
-        </div>
-      </div>
-
-      <div className="flex pb-4 gap-2">
-        <h4 className="w-24">Foto</h4>
-        <div className="flex flex-col flex-1">
-          <h5 className="text-[10px] font-semibold pb-1 border-b-2">Update kondisi banjir kanal saat ini</h5>
-          <p className="text-[8px] ">19 jam yang lalu</p>
-          <p className="pt-3 text-[10px]">Baca selengkapnya...</p>
-        </div>
-      </div>
-    </div>
-  )
-}
+import posts from "@/lib/dummyData";
 
 const Laporan = () => {
   return (
@@ -42,7 +11,9 @@ const Laporan = () => {
       <div className="flex pb-4 gap-2">
         <h4 className="w-24">Foto</h4>
         <div className="flex flex-col flex-1">
-          <h5 className="text-[10px] font-semibold pb-1 border-b-2">100kg sampah plastik</h5>
+          <h5 className="text-[10px] font-semibold pb-1 border-b-2">
+            100kg sampah plastik
+          </h5>
           <p className="text-[8px] ">1 jam yang lalu</p>
           <p className="pt-3 text-[10px]">Baca selengkapnya...</p>
         </div>
@@ -51,20 +22,25 @@ const Laporan = () => {
       <div className="flex pb-4 gap-2">
         <h4 className="w-24">Foto</h4>
         <div className="flex flex-col flex-1">
-          <h5 className="text-[10px] font-semibold pb-1 border-b-2">100kg sampah plastik</h5>
+          <h5 className="text-[10px] font-semibold pb-1 border-b-2">
+            100kg sampah plastik
+          </h5>
           <p className="text-[8px] ">1 jam yang lalu</p>
           <p className="pt-3 text-[10px]">Baca selengkapnya...</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default function Page() {
   const [activeTab, setActiveTab] = useState("postingan");
+  const [showAllPosts, setShowAllPosts] = useState(false);
+
+  const MAX_POSTS = 3;
 
   return (
-    <main className="p-6 ">
+    <main className="p-6">
       <div className="flex flex-col px-6 pt-6 rounded-t-md bg-color5 shadow-lg border-b-2">
         <div className="flex pb-5 gap-4">
           {/* Foto profil */}
@@ -90,12 +66,16 @@ export default function Page() {
         <div>
           <h2 className="font-semibold text-sm">Inspektur Alviano Sigmatus</h2>
           <p className="font-light text-xs">@sigmavin</p>
-          <p className="pt-1 text-xs">Inspektur kebersihan lokal area Gayamsari</p>
+          <p className="pt-1 text-xs">
+            Inspektur kebersihan lokal area Gayamsari
+          </p>
         </div>
 
         <div className="flex my-3 text-[11px] gap-2">
           <Link href="">Edit Profil</Link>
-          <Link href="" className="text-red-500">Keluar</Link>
+          <Link href="" className="text-red-500">
+            Keluar
+          </Link>
         </div>
 
         <div className="flex my-2 px-10 gap-2 text-xs font-medium justify-between">
@@ -119,8 +99,30 @@ export default function Page() {
       </div>
 
       <div className="flex flex-col px-6 pt-4 rounded-b-md bg-color5 shadow-lg">
-        {activeTab === "postingan" && <Postingan/>}
-        {activeTab === "laporan" && <Laporan/>}
+        {activeTab === "postingan" && (
+          <div className="grid grid-rows-2 gap-4">
+            {(!showAllPosts ? posts.slice(0, MAX_POSTS) : posts).map((post) => (
+              <ProfilePostCard key={post.id} post={post} />
+            ))}
+            {posts.length > MAX_POSTS && !showAllPosts && (
+              <button
+                className="text-blue-500 mt-4 underline"
+                onClick={() => setShowAllPosts(true)}
+              >
+                Lihat Semua
+              </button>
+            )}
+            {showAllPosts && (
+              <button
+                className="text-blue-500 mt-4 underline"
+                onClick={() => setShowAllPosts(false)}
+              >
+                Tampilkan Lebih Sedikit
+              </button>
+            )}
+          </div>
+        )}
+        {activeTab === "laporan" && <Laporan />}
       </div>
     </main>
   );

bb0f624cd29a5bb3ee8b0f248938e974063ec579
MDAlviano <mdalviano.21122@gmail.com>
2025-01-06 14:38:56 +0700

    add page map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import { MapPin } from "@geist-ui/icons";
+import exampleMapImg from "../../../public/images/example-map.png";
+
 export default function Page() {
-    return (
-        <div>Map</div>
-    );
+  return (
+    <div className="relative w-full h-screen">
+      <div className="relative w-full h-full overflow-hidden">
+        <Image src={exampleMapImg} alt="example gmap img" className="w-full h-full object-cover" />
+        <button id="fab-map" className="absolute bottom-16 right-8 lg:bottom-20 lg:right-12 p-3 rounded-lg bg-color8 border border-color1 shadow-lg hover:bg-opacity-90 transition-all">
+          <MapPin color="#000" size={28} />
+        </button>
+      </div>
+    </div>
+  );
 }

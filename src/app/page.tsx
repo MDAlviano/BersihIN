556e72ff86dd2834b533d0f88be3f5c9945358b0
hnifalii <ankso30561@gmail.com>
2025-01-06 21:47:27 +0700

    WIP: home (ketiduran)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 
 import FaqItem from "@/components/FaqItem";
 import { useAuth } from "@/context/AuthContext";
+import { ArrowRight } from "@geist-ui/icons";
 import Link from "next/link";
 import { faqs } from "@/lib/dummyData";
+import Image from "next/image";
 
 export default function Home() {
   const { isLogged } = useAuth();
@@ -42,16 +44,29 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section id="map" className="px-10 py-14">
-        <h1>Cek Kondisi Terkini</h1>
+
+      {/* Map */}
+      <section id="map" className="flex flex-col gap-1 px-10 py-14">
+        <h1 className="text-2xl font-semibold">Cek Kondisi Terkini</h1>
+        <h4 className="text-base font-light">
+          Cari tahu bagaimana kondisi terbaru di setiap wilayah!
+        </h4>
+        <Image
+          src="/images/wilayah.png"
+          width={334}
+          height={201}
+          layout="responsive"
+          alt="map"
+          className="p-2"
+        />
+        <Link href="/map">
+          <button className="flex  items-center gap-2 w-auto py-3 px-5 bg-color1 rounded-md text-sm text-color7">
+            Cek di sini <ArrowRight size={20}/>
+          </button>
+        </Link>
       </section>
 
       {/* FAQ */}
-      {/* <section id="faq" className="">
-        <div className="">
-
-        </div>
-      </section> */}
       <section id="faq" className="pt-10">
         <div className="flex flex-col mx-6 my-10 px-6 py-10 gap-8 rounded-xl items-center bg-color1 shadow transition duration-100">
           <h1 className="text-4xl font-bold text-color7 text-center">FAQ</h1>

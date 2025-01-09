48499a17f99a2455ed83bf4be5927d4354097c8e
hnifalii <ankso30561@gmail.com>
2025-01-09 15:40:02 +0700

    add laporan

--- src/app/map/details/add/page.tsx
+++ src/app/map/details/add/page.tsx
@@ -1,5 +1,54 @@
 export default function Page() {
-    return (
-        <div>Add new report</div>
-    );
+  return (
+    <div className="flex flex-col lg:bg-color8 rounded-2xl lg:border border-color1 my-10 mx-auto w-fit p-8 lg:p-14">
+      <h1 className="lg:mx-auto text-4xl font-semibold">Laporan</h1>
+      <h4 className="lg:mx-auto text-lg font-medium mt-2">
+        Laporkan sampah-sampah yang ada di sekitarmu!
+      </h4>
+      {/* form section */}
+      <form className="flex flex-col gap-5 p-7 my-9 lg:mx-5 bg-color5 rounded-md border border-color1">
+        <div
+          id="input-image-placeholder"
+          className="flex w-full h-52 lg:h-80 bg-color5 rounded-lg border border-color1"
+        >
+          <h6 className="m-auto text-[#8A8A8A]">Upload gambar</h6>
+        </div>
+        <input
+          id="input-title"
+          type="text"
+          placeholder="Judul"
+          required
+          className="py-3 px-7 rounded-lg border border-color1 bg-color5 focus:outline-none focus:border-2"
+        />
+        <input
+          id="input-index"
+          type="number"
+          min={1}
+          max={100}
+          placeholder="Indeks (1-100)"
+          required
+          className="py-3 px-7 rounded-lg border border-color1 bg-color5 focus:outline-none focus:border-2"
+        />
+        <textarea
+          id="input-description"
+          name=""
+          rows={5}
+          placeholder="Tambahkan deskripsi..."
+          className="py-3 px-7 rounded-lg border border-color1 bg-color5 focus:outline-none focus:border-2"
+        ></textarea>
+        <p className="text-[#8A8A8A]">
+          <span className="font-semibold">Note:</span> Kamu harus menunggu
+          beberapa saat sebelum di unggah ke publik, karena akan di review
+          terlebih dahulu oleh tim reviewer.
+        </p>
+        <button
+          id="btn-submit"
+          type="submit"
+          className="w-full bg-color1 py-3 text-white font-semibold rounded-xl my-4 lg:my-8"
+        >
+          Upload
+        </button>
+      </form>
+    </div>
+  );
 }
